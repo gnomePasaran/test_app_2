888855f8adfdf61b13f87bb79b3e276b29f4af74
Alexander Ivahnenko <alexander_i@fast-report.com>
2018-02-01 21:02:24 +0300

    add User Controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  use_doorkeeper
+  devise_for :users
+
+  root to: 'users#index'
+
+  resources :users, only: :index
 end
