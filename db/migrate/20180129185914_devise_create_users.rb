684b66ccefedc758171a799b1b768ee85890be83
Alexander Ivahnenko <alexander_i@fast-report.com>
2018-02-01 20:57:33 +0300

    add doorkeeper configs

--- db/migrate/20180129185914_devise_create_users.rb
+++ db/migrate/20180129185914_devise_create_users.rb
@@ -4,8 +4,10 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.1]
   def change
     create_table :users do |t|
       ## Database authenticatable
+      t.boolean :admin,             null: false, default: false
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
+
 
       ## Recoverable
       t.string   :reset_password_token
