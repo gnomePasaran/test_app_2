b1731d992954e4d9cef0c3e314063fa0ba3f5af1
Alexander Ivahnenko <alexander_i@fast-report.com>
2018-02-02 01:35:55 +0300

    add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,14 @@
-User.create!(
+admin = User.create!(
   admin: true,
   email: 'admin@test.com',
   password: 'password',
   password_confirmation: 'password',
 )
+user = User.create!(
+  email: 'user@test.com',
+  password: 'password',
+  password_confirmation: 'password',
+)
+
+2.times.each { Post.create(user: admin, text: 'Some text') }
+3.times.each { Post.create(user: user, text: 'Some text') }
