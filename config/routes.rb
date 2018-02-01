502d7064e90d2ab28963ee3ce5450d26dce20776
Alexander Ivahnenko <alexander_i@fast-report.com>
2018-02-02 01:39:00 +0300

    add route for api Post

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,10 @@ Rails.application.routes.draw do
   root to: 'users#index'
 
   resources :users, only: :index
+
+  namespace :api do
+    namespace :v1 do
+      resources :posts, only: [:index, :create]
+    end
+  end
 end
