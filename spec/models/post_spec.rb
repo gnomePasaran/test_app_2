d3c5133d4ff1ee5e40af8fa53f909a2484463818
Alexander Ivahnenko <alexander_i@fast-report.com>
2018-02-01 21:39:49 +0300

    add specs for models

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # it { should belong_to(:user) }
+  # it { is_expected.to belong_to(:user) }
+  it { should validate_presence_of(:text) }
+  it { should validate_length_of(:text).is_at_least(3).is_at_most(140) }
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,4 +4,6 @@ RSpec.describe User, type: :model do
   it { should validate_presence_of(:email) }
   it { should validate_uniqueness_of(:email).ignoring_case_sensitivity }
   it { should validate_presence_of(:password) }
+
+  # it { should have_many(:posts) }
 end
