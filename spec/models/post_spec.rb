fd51da3f5d28c4ffed9992a227290454b7509028
Alexander Ivahnenko <alexander_i@fast-report.com>
2018-02-02 01:36:43 +0300

    fix models specs

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  # it { should belong_to(:user) }
-  # it { is_expected.to belong_to(:user) }
+  it { should belong_to(:user) }
+
   it { should validate_presence_of(:text) }
   it { should validate_length_of(:text).is_at_least(3).is_at_most(140) }
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,5 +5,5 @@ RSpec.describe User, type: :model do
   it { should validate_uniqueness_of(:email).ignoring_case_sensitivity }
   it { should validate_presence_of(:password) }
 
-  # it { should have_many(:posts) }
+  it { should have_many(:posts) }
 end
