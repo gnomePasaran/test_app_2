efc9c37af85521680003fc844c902714c1268657
Alexander Ivahnenko <alexander_i@fast-report.com>
2018-02-02 01:39:50 +0300

    chenge in Devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,8 @@
+require "application_responder"
+
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
+  self.responder = ApplicationResponder
+  respond_to :html
+
   protect_from_forgery with: :exception
 end
