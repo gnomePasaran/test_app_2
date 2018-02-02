a4405bc95df68e728b4f0eaa43ec958114d2c464
Alexander Ivahnenko <alexander_i@fast-report.com>
2018-02-02 23:02:04 +0300

    add most_liked method for Like model

--- app/models/like.rb
+++ app/models/like.rb
@@ -3,4 +3,8 @@ class Like < ApplicationRecord
   belongs_to :user
 
   validates :post_id, uniqueness: { scope: :user_id }
+
+  scope :most_liked, -> do
+    select('post_id, count(post_id)').group('post_id').order('count(post_id) desc').pluck('post_id, count(post_id)').first(5)
+  end
 end
--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -5,4 +5,28 @@ RSpec.describe Like, type: :model do
   it { should belong_to(:user) }
 
   it { should validate_uniqueness_of(:post_id).scoped_to(:user_id) }
+
+  describe '.most_liked' do
+    let!(:posts) { create_list(:post, 5) }
+    let!(:post) { create(:post) }
+    before { posts.map.with_index { |post, i| create_list(:like, i + 2, post: post) } }
+    let(:result) { posts.map { |post| [post.id, post.likes.count] } }
+
+    it 'does not contains post without likes' do
+      expect(Like.most_liked).to_not include [post.id, post.likes.count]
+    end
+
+    it 'does not contains post with lesser the 5th likes count' do
+      create(:like, post: post)
+      expect(Like.most_liked).to_not include [post.id, post.likes.count]
+    end
+
+    it 'return most liked post' do
+      expect(Like.most_liked).to eq result.reverse
+    end
+
+    it "return most liked post's like count" do
+      expect(Like.most_liked).to eq result.reverse
+    end
+  end
 end
