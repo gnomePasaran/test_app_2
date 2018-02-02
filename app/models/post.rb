2a0be65624bae8699fe62f2a6ee6a02a5dc3e90d
Alexander Ivahnenko <alexander_i@fast-report.com>
2018-02-02 23:01:19 +0300

    add most_posting method to Post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,4 +5,8 @@ class Post < ApplicationRecord
 
   validates :text, presence: true
   validates :text, length: { maximum: 140 }
+
+  scope :most_posting, -> {
+    select('user_id, count(user_id)').group('user_id').order('count(user_id) desc').pluck('user_id, count(user_id)').first(5)
+  }
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -6,4 +6,28 @@ RSpec.describe Post, type: :model do
 
   it { should validate_presence_of(:text) }
   it { should validate_length_of(:text).is_at_most(140) }
+
+  describe '.most_posting' do
+    let!(:users) { create_list(:user, 5) }
+    let!(:user) { create(:user) }
+    before { users.map.with_index { |u, i| create_list(:post, i + 2, user: u) } }
+    let(:result) { users.map { |u| [u.id, u.posts.count] } }
+
+    it 'does not contains user without posts' do
+      expect(Post.most_posting).to_not include [user.id, user.posts.count]
+    end
+
+    it 'does not contains user with lesser the 5th posts count' do
+      create(:post, user: user)
+      expect(Post.most_posting).to_not include [user.id, user.posts.count]
+    end
+
+    it 'return most posting user' do
+      expect(Post.most_posting).to eq result.reverse
+    end
+
+    it "return most posting user's post count" do
+      expect(Post.most_posting).to eq result.reverse
+    end
+  end
 end
