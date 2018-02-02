87195eb5d4d84b1400b11f40fd3e707a2a88f887
Alexander Ivahnenko <alexander_i@fast-report.com>
2018-02-02 23:02:27 +0300

    add acceptense spec for info page

--- spec/feature/user_info_spec.rb
+++ spec/feature/user_info_spec.rb
@@ -11,11 +11,37 @@ feature 'To see Top-5 info', %q{
 
   background do
     users.map { |u| create(:post, user: u) }
+    Post.all.map.with_index do |post, i|
+      users.map { |u| create(:like, user: u, post: post) unless post.user_id == u.id }
+    end
+    visit root_path
   end
 
-  scenario 'Guest try to see Top-5 most posting users' do
-    visit root_path
-    expect(page).to have_content users.first.email
-    expect(page).to_not have_content user_without_posts.last.email
+  describe 'Guest try to see Top-5' do
+    scenario 'the most posting users' do
+      expect(page).to have_content users.first.email
+      expect(page).to_not have_content user_without_posts.email
+    end
+
+    scenario 'count of posts of the most posting users' do
+      expect(page).to have_content users.first.posts.count
+    end
+  end
+
+  describe 'Guest try to see Top-5 users' do
+    given(:most_liked_users) { User.most_liked_users }
+    scenario 'with the most liked posts and posts itself' do
+      expect(page).to have_content most_liked_users.first[1].email
+      expect(page).to have_content most_liked_users.first[0].text
+      expect(page).to have_content most_liked_users.first[2]
+    end
+  end
+
+  describe 'Guest try to see Top-5 users' do
+    given(:most_average_users) { User.most_average_users }
+    scenario 'with the most average rating' do
+      expect(page).to have_content most_average_users.first[0].email
+      expect(page).to have_content most_average_users.first[1]
+    end
   end
 end
